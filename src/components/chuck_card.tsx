8cccf94c8e49bf4a324cdf913b55a935b08d019b
Tom <admin@Admins-MBP.lan>
2023-02-02 16:44:39 +0000

    Complete Requirement 1 - Chuck Card

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,13 +1,17 @@
 // 💡 You can import assets like images directly and use them in your components:
 import ChuckImage from '../assets/chuck_norris.jpg';
 
-const ChuckCard = () =>
+interface HeaderProps{
+	header: string;
+}
+
+const ChuckCard: React.FC<HeaderProps> = ({header}) =>
 	// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
 	// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
 	<>
-		<h2>{ }</h2>
+		<h2>{header}</h2>
 
-		<img src={'Chuck image here!'} alt='A handsome man' />
+		<img src={ChuckImage} alt='A handsome man' />
 	</>
 
 
